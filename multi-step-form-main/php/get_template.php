cab0f79795d74bf4e813d5b014707d2d1047eace
MiguelGzE <miguel-glezes@hotmail.com>
2023-02-04 00:14:20 -0600

    fix: Cambio en Acess controll allow origin

--- multi-step-form-main/php/get_template.php
+++ multi-step-form-main/php/get_template.php
@@ -2,8 +2,8 @@
 
 try {
 
-    // header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+    header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
     header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
     header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
